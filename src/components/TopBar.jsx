cb5bef3f6db36ccd9351a8db09662d299614659c
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2021-12-11 00:37:13 +0100

    started working on nav logo

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,12 +4,53 @@ const TopNav = styled.div`
   height: 10vh;
   width: 100vw;
   text-align: center;
+  display: flex;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Logo = styled.div`
+  margin-left: 2rem;
+  h1 {
+    font-weight: 200;
+    font-size: 3rem;
+  }
+  #nastavak {
+    display: none;
+    opacity: 0;
+    transition: all 0.3s ease-in;
+  }
+  #glavni {
+    margin: 0 0.5rem;
+    transition: all 0.3s ease-in;
+
+    &:hover #nastavak {
+      display: inline;
+      opacity: 1;
+    }
+  }
 `;
 function TopBar() {
   return (
     <>
       <TopNav>
-        <h1>Nav-traka</h1>
+        <Left>
+          <Logo>
+            <h1>
+              &lt;
+              <span id="glavni">
+                B<span id="nastavak">tech</span>
+              </span>
+              &gt;{" "}
+            </h1>
+          </Logo>
+        </Left>
+        <Right></Right>
       </TopNav>
     </>
   );
