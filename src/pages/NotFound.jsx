7098c2e79a76a877bac537dc353b4f92daeb811e
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2021-12-30 19:23:43 +0100

    stabel base for all pages fix

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import AnimatePage from "../utils/AnimatePage";
+import styled from "styled-components";
+import { phone } from "../utils/responsive";
+const Wrapper = styled.div`
+  max-width: 71rem;
+  margin: auto;
+    ${phone({
+      width: "100%",
+    })}
+    
+  }
+`;
 function NotFound() {
   return (
     <AnimatePage>
-      <div>
+      <Wrapper>
         <h1>404 not found</h1>
-      </div>
+      </Wrapper>
     </AnimatePage>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import AnimatePage from "../utils/AnimatePage";
-
+import styled from "styled-components";
+import { phone } from "../utils/responsive";
+const Wrapper = styled.div`
+  max-width: 71rem;
+  margin: auto;
+    ${phone({
+      width: "100%",
+    })}
+    
+  }
+`;
 function Projects() {
   return (
     <AnimatePage>
-      <div>
+      <Wrapper>
         <h1>projects page</h1>
-      </div>
+      </Wrapper>
     </AnimatePage>
   );
 }
