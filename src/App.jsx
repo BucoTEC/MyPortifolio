d0935c7efc9f8beda7c9e0e5688719783cc2de84
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-01 23:35:30 +0200

    fix react import in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Home from "./pages/Home";
 import TopBar from "./ui/TopBar";
 import Footer from "./ui/Footer";
@@ -9,25 +10,25 @@ import styled from "styled-components";
 import { AnimatePresence } from "framer-motion";
 import NotFound from "./pages/NotFound";
 const Wrapper = styled.div`
-  width: 100vw;
+	width: 100vw;
 `;
 function App() {
-  const location = useLocation();
-  return (
-    <Wrapper>
-      <TopBar />
-      <AnimatePresence>
-        <Routes key={location.key} location={location}>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={<Skills />} />
-        </Routes>
-      </AnimatePresence>
-      <Footer />
-    </Wrapper>
-  );
+	const location = useLocation();
+	return (
+		<Wrapper>
+			<TopBar />
+			<AnimatePresence>
+				<Routes key={location.key} location={location}>
+					<Route path="*" element={<NotFound />} />
+					<Route path="/" element={<Home />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/skills" element={<Skills />} />
+				</Routes>
+			</AnimatePresence>
+			<Footer />
+		</Wrapper>
+	);
 }
 
 export default App;
